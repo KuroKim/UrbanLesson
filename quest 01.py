8155676b1bce5a2a0cffb1203345c111bb921677
Pavel <KuroKimala@gmail.com>
2024-08-16 20:36:42 +0700

    +  hard quest

---
+++ quest 01.py
@@ -0,0 +1,25 @@
+# Задание "Средний балл":
+# Вам необходимо решить задачу из реальной жизни: "школьные учителя устали подсчитывать вручную средний балл каждого ученика, поэтому вам предстоит автоматизировать этот процесс":
+#
+# На вход даны следующие данные:
+# Список: grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+# Множество: students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+#
+# Список grades содержит списки оценок для каждого ученика в алфавитном порядке.
+# Например: 'Aaron' - [5, 3, 3, 5, 4]
+# Множество students содержит неупорядоченную последовательность имён всех учеников в классе.
+#
+# Напишите программу, которая составляет словарь, используя grades и students, где ключом будет имя ученика, а значением - его средний балл.
+
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+
+students_list = list(students)
+students_list.sort()
+
+average_grades = {}
+for student in students_list:
+    total_grade = sum(grades[students_list.index(student)])
+    average_grades[student] = total_grade / len(grades[students_list.index(student)])
+
+print(average_grades)

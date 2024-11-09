a44877afdcc5ccba41c3a231ca840e094f193b98
KuroKim <KuroKimala@gmail.com>
2024-11-09 13:58:34 +0700

    + Модуль: 14 | Задача: 1 + БД

---
+++ module 14/module_14_1.py
@@ -0,0 +1,92 @@
+# Задача "Первые пользователи":
+# Создайте файл базы данных not_telegram.db и подключитесь к ней, используя встроенную библиотеку sqlite3.
+# Создайте объект курсора и выполните следующие действия при помощи SQL запросов:
+# Создайте таблицу Users, если она ещё не создана. В этой таблице должны присутствовать следующие поля:
+# id - целое число, первичный ключ
+# username - текст (не пустой)
+# email - текст (не пустой)
+# age - целое число
+# balance - целое число (не пустой)
+# Заполните её 10 записями:
+# User1, example1@gmail.com, 10, 1000
+# User2, example2@gmail.com, 20, 1000
+# User3, example3@gmail.com, 30, 1000
+# ...
+# User10, example10@gmail.com, 100, 1000
+# Обновите balance у каждой 2ой записи начиная с 1ой на 500:
+# User1, example1@gmail.com, 10, 500
+# User2, example2@gmail.com, 20, 1000
+# User3, example3@gmail.com, 30, 500
+# ...
+# User10, example10@gmail.com, 100, 1000
+# Удалите каждую 3ую запись в таблице начиная с 1ой:
+# User2, example2@gmail.com, 20, 1000
+# User3, example3@gmail.com, 30, 500
+# User5, example5@gmail.com, 50, 500
+# ...
+# User9, example9@gmail.com, 90, 500
+#
+# Сделайте выборку всех записей при помощи fetchall(), где возраст не равен 60 и выведите их в консоль в следующем формате (без id):
+# Имя: <username> | Почта: <email> | Возраст: <age> | Баланс: <balance>
+
+
+import sqlite3
+
+conn = sqlite3.connect('not_telegram.db')
+cursor = conn.cursor()
+
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS Users (
+        id INTEGER PRIMARY KEY,
+        username TEXT NOT NULL,
+        email TEXT NOT NULL,
+        age INTEGER NOT NULL,
+        balance INTEGER NOT NULL
+    )
+''')
+
+users = [
+    ('User1', 'example1@gmail.com', 10, 1000),
+    ('User2', 'example2@gmail.com', 20, 1000),
+    ('User3', 'example3@gmail.com', 30, 1000),
+    ('User4', 'example4@gmail.com', 40, 1000),
+    ('User5', 'example5@gmail.com', 50, 1000),
+    ('User6', 'example6@gmail.com', 60, 1000),
+    ('User7', 'example7@gmail.com', 70, 1000),
+    ('User8', 'example8@gmail.com', 80, 1000),
+    ('User9', 'example9@gmail.com', 90, 1000),
+    ('User10', 'example10@gmail.com', 100, 1000)
+]
+
+cursor.executemany('''
+    INSERT INTO Users (username, email, age, balance)
+    VALUES (?, ?, ?, ?)
+''', users)
+conn.commit()
+
+cursor.execute('''
+    UPDATE Users
+    SET balance = 500
+    WHERE id % 2 = 1
+''')
+conn.commit()
+
+# Удаляем каждую 3ю запись
+cursor.execute('''
+    DELETE FROM Users
+    WHERE id IN (SELECT id FROM Users WHERE (rowid - 1) % 3 = 0)
+''')
+conn.commit()
+
+cursor.execute('''
+    SELECT username, email, age, balance
+    FROM Users
+    WHERE age != 60
+''')
+
+users = cursor.fetchall()
+
+for user in users:
+    print(f'Имя: {user[0]} | Почта: {user[1]} | Возраст: {user[2]} | Баланс: {user[3]}')
+
+conn.close()

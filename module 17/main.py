456c0144d3af8c88e88574865a9d03d2ac6b8f97
KuroKim <KuroKimala@gmail.com>
2024-12-05 11:52:54 +0700

    + Модуль: 17 | Задача: 1

---
+++ module 17/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from routers.user import router as user_router
+from routers.task import router as task_router
+
+app = FastAPI()
+
+
+@app.get("/")
+def welcome():
+    return {"message": "Welcome to Taskmanager"}
+
+
+app.include_router(user_router)
+app.include_router(task_router)
+
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host='127.0.0.1', port=8000)
